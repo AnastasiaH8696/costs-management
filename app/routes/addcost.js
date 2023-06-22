13c8981ea1e16c72b4c4e1b18f0429f51ba0ad1c
shirelbitan277@gmail.com <shirelbitan277@gmail.com>
2023-06-22 12:08:19 +0300

    editing

--- app/routes/addcost.js
+++ app/routes/addcost.js
@@ -7,17 +7,31 @@ Dev2 name: Shirel Bitan
 Dev2 ID: 209322395
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET render addCost view */
-router.get('/', function(req, res, next) {
-  res.render('addcost');
+router.get("/", function (req, res, next) {
+  res.render("addcost");
 });
 
 /* POST new cost item */
-router.post('/', function(req, res, next) {
-  // TODO
+router.post("/", async (req, res, next) => {
+  const { id, user_id, year, month, day, description, category, sum } =
+    req.body;
+
+  const newCost = new costs({
+    id,
+    user_id,
+    year,
+    month,
+    day,
+    description,
+    category,
+    sum,
+  });
+
+  const savedCost = await newCost.save();
 });
 
 module.exports = router;
